3a2911e45b6b765bbdc138c65eeda108b654a596
Alex Kasema <lexxmulwa@gmail.com>
2024-07-04 01:57:28 -0700

    Implement an is_valid function that checks validity of a password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,11 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check if the password is equal to the hashed password """
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    else:
+        return False
