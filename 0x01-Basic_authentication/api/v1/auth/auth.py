cfd31021073044c6b5ca31bcb33355f9a4151f23
Alex Kasema <lexxmulwa@gmail.com>
2024-07-08 07:15:38 -0700

    Define which routes don't need authentication. In Auth that returns True if the path is not in the list of strings excluded_paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,19 @@ from typing import List, TypeVar
 class Auth:
     """ Manage API Authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ check for authentication requirement """
-        return False
+        """ check if path requires authentication """
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+
+        if path[-1] == '/':
+            path = path[:-1]
+
+        path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Checks Authorization header """
