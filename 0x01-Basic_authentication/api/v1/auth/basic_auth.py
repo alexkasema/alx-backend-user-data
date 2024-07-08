ad510b47abc3c59c45b7703c9d68b87865d93eb7
Alex Kasema <lexxmulwa@gmail.com>
2024-07-08 11:30:54 -0700

    Improve the method def extract_user_credentials(self, decoded_base64_authorization_header) to allow password with :

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -50,7 +50,7 @@ class BasicAuth(Auth):
                 isinstance(decoded_base64_authorization_header, str) and
                 ':' in decoded_base64_authorization_header):
             return(None, None)
-        email, password = decoded_base64_authorization_header.split(":")
+        email, password = decoded_base64_authorization_header.split(":", 1)
 
         return (email, password)
 
