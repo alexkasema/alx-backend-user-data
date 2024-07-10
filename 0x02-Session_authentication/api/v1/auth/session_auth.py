50b2e5aaf5beee32e2d947438618cd05eb8dc349
Alex Kasema <lexxmulwa@gmail.com>
2024-07-10 05:06:16 -0700

    Create a session. An instance method that creates a Session ID for a user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 """ Session authentication """
 
+from uuid import uuid4
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """ Handle Session Authentication """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ create a session ID for a user_id """
+
+        if not(user_id and isinstance(user_id, str)):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
