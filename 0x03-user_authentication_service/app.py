a7d20044da6ea7c5529755abb2fc30bbece6ddd2
Alex Kasema <lexxmulwa@gmail.com>
2024-07-16 02:49:13 -0700

    Set up A Basic Flask app.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 AUTH = Auth()
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route("/", methods=['GET'])
 def index() -> str:
     """ Return a JSON payload """
 
